0a1146a37a4f0bfe9f77f1017db4bb2d0e48fc58
Enes Yılmaz <yenesyilmaz11@hotmail.com>
2021-01-21 22:45:41 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+        Console.WriteLine("Sayın {0} {1} {2} ID numaralı hesabınız eklenmiştir",customer.Name,customer.Surname,customer.Id);
+        }
+
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine("Sayın {0} {1} {2} ID numaralı hesabınız silinmiştir", customer.Name, customer.Surname, customer.Id);
+        }
+
+        public void CustomerList(Customer customer)
+        {
+            Console.WriteLine("ID: {0}\nAdı:{1}\nSoyadı:{2}\nBakiye:{3}\n", customer.Id, customer.Name, customer.Surname, customer.Balance);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id = 101101,
+                Balance = 500000,
+                Name = "Enes",
+                Surname = "Yılmaz"
+            };
+
+            Customer customer2 = new Customer()
+            {
+                Id = 101011,
+                Balance = 1000000,
+                Name = "Haktan",
+                Surname = "Çınar"
+            };
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CustomerAdd(customer1);
+            customerManager.CustomerList(customer1);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
